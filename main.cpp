c1c57396435edd428d2178aa667aab8087348998
JanSlusarczyk <252832@edu.p.lodz.pl>
2023-11-29 12:55:04 +0100

    nowa metoda policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 
+int policz(int a, int b){
+    return a + b;
+}
+
 int main() {
     std::cout << "Hello, Word!" << std::endl;
+    std::cout << "W pierwszym branchu 2 + 3 = " << policz(2, 3) << std::endl;
+
     return 0;
 }
